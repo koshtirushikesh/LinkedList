4a1f823353d2b18b8cf41789f45378ffe8c9d455
koshtirushikesh <koshtirushikesh3@gmail.com>
2023-02-23 12:09:23 +0530

    create a simple Linked List of 56, 30 and 70

---
+++ LinkedListProject/LinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int Data)
+        {
+            Node node = new Node();
+            node.Data = Data;
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+            }
+            Console.Write("Linked list is: ");
+            while (temp != null)
+            {
+                Console.Write(temp.Data + " ");
+                temp = temp.Next;
+            }
+            Console.WriteLine("\n");
+        } 
+    }
+}
---
+++ LinkedListProject/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        //public int Data;
+        //public Node Next;
+        //public Node(int Data) 
+        //{
+        //    this.Data = Data;
+        //}
+
+        public int Data { get; set; }
+        public Node Next { get; set; }
+    }
+}
